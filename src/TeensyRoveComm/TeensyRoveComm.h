7c757e224848ee67f4c95997447fa9a7ccbf4c7c
antrob25 <antrobles254@gmail.com>
2021-10-21 02:17:24 -0500

    First intial Teensy RoveComm Integration, still need to organize the folder without causing eternal blindness

---
+++ src/TeensyRoveComm/TeensyRoveComm.h
@@ -0,0 +1,9 @@
+#ifndef TeensyRoveComm
+#define TeensyRoveComm
+
+#include "TeensyRoveCommEthernet/TeensyRoveCommEthernet.h"
+#include "TeensyRoveCommEthernetUDP/TeensyRoveCommEthernetUdp.h"
+#include "TeensyRoveCommEthernetTCP/TeensyRoveCommEthernetTCP.h"
+#include "TeensyRoveCommSerial/TeensyRoveCommSerial.h"
+
+#endif
