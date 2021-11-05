bda528bab2cf00a19e9e7bf416c734ad2bc455df
antrob25 <antrobles254@gmail.com>
2021-11-05 00:46:07 -0500

    Removed extra files for Teensy

--- src/TeensyRoveComm/TeensyRoveComm.h
+++
@@ -1,9 +0,0 @@
-#ifndef TeensyRoveComm
-#define TeensyRoveComm
-
-#include "TeensyRoveCommEthernet/TeensyRoveCommEthernet.h"
-#include "TeensyRoveCommEthernetUDP/TeensyRoveCommEthernetUdp.h"
-#include "TeensyRoveCommEthernetTCP/TeensyRoveCommEthernetTCP.h"
-#include "TeensyRoveCommSerial/TeensyRoveCommSerial.h"
-
-#endif
