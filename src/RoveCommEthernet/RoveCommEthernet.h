d2dbfb1998608ff59fb52646f225c4cca0dd0dcc
OcelotEmpire <hobbz.pi@gmail.com>
2025-06-12 13:10:26 -0500

    slight refactor and documentation

--- src/RoveCommEthernet/RoveCommEthernet.h
+++ src/RoveCommEthernet/RoveCommEthernet.h
@@ -16,23 +16,48 @@
 #include "RoveCommEthernetTCP.h"
 #include "RoveCommEthernetUDP.h"
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * @brief A wrapper for a RoveCommEthernetTCP and a RoveCommEthernetUDP instance.
+ * 
+ */
 class RoveCommEthernet {
 public:
     RoveCommEthernetTCP TCP;
     RoveCommEthernetUDP UDP;
 
-    /////begin//////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Initiate RoveComm with the given address
+    /**
+     * @brief Initialize the ethernet stack and begin listening for connections.
+     * 
+     * @param ip The static IP Address this board will have.
+     * @param mac Any 6 bytes, so long as they are unique to this board.
+     * @param udpPort Port to use for UDP communication. Default is RC_ROVECOMM_ETHERNET_UDP_PORT
+     * @param tcpPort Port to use for TCP communication. Default is RC_ROVECOMM_ETHERNET_TCP_PORT
+     */
     void begin(IPAddress ip, uint16_t udpPort=RC_ROVECOMM_ETHERNET_UDP_PORT, uint16_t tcpPort=RC_ROVECOMM_ETHERNET_TCP_PORT);
+    
+    /**
+     * @brief Initialize the ethernet stack and begin listening for connections.
+     * 
+     * @param ip The static IP Address this board will have.
+     * @param udpPort Port to use for UDP communication. Default is RC_ROVECOMM_ETHERNET_UDP_PORT
+     * @param tcpPort Port to use for TCP communication. Default is RC_ROVECOMM_ETHERNET_TCP_PORT
+     */
     void begin(IPAddress ip, uint8_t *mac, uint16_t udpPort=RC_ROVECOMM_ETHERNET_UDP_PORT, uint16_t tcpPort=RC_ROVECOMM_ETHERNET_TCP_PORT);
 
-    /////read///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Reads the latest TCP data and, if none, the latest UDP packet
-    // Returns true if a packet was read
-    // Returns false if no packet was read. The packet's dataId field will also be set to RC_ROVECOMM_NO_DATA_DATA_ID
+    /**
+     * @brief Read the latest TCP data and, if none, the latest UDP packet off the network.
+     * 
+     * @param dest A RoveCommPacket object to store the read packet into. This prevents an expensive memory copy.
+     * @return true if a packet was read.
+     * @return false if no packet was read. The packet's dataId field will also be set to RC_ROVECOMM_NO_DATA_DATA_ID
+     */
     bool read(RoveCommPacket &dest);
-    // Convenience method to follow old code pattern. Prefer read(packet) instead.
+
+    /**
+     * @brief Convenience method to follow an old code pattern. Prefer read(packet) instead.
+     * 
+     * @return RoveCommPacket 
+     */
     RoveCommPacket read() {
         RoveCommPacket packet;
         read(packet);
@@ -41,9 +66,13 @@ public:
 
     ///// CONVENIENCE METHODS ////////////////////////////////////////////////////////////////////////////////////////////
 
-    /////write////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Array entry write
-    // Overloaded for each data type
+    /**
+     * @brief Send data to all subscribers via UDP. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_DRIVELEFTRIGHT_DATA_ID
+     * @param dataCount How many elements are in the data array.
+     * @param data The data to send.
+     */
     void write(uint16_t dataId, uint16_t dataCount, const uint8_t  *data) { UDP.write(dataId, dataCount, data); }
     void write(uint16_t dataId, uint16_t dataCount, const uint16_t *data) { UDP.write(dataId, dataCount, data); }
     void write(uint16_t dataId, uint16_t dataCount, const uint32_t *data) { UDP.write(dataId, dataCount, data); }
@@ -53,8 +82,13 @@ public:
     void write(uint16_t dataId, uint16_t dataCount, const float    *data) { UDP.write(dataId, dataCount, data); }
     void write(uint16_t dataId, uint16_t dataCount, const double   *data) { UDP.write(dataId, dataCount, data); }
     void write(uint16_t dataId, uint16_t dataCount, const char     *data) { UDP.write(dataId, dataCount, data); }
-    // Single-value write
-    // Overloaded for each data type
+
+    /**
+     * @brief Convenience method for sending a single value to all subscribers via UDP. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_WATCHDOGOVERRIDE_DATA_ID
+     * @param data The datum to send.
+     */
     void write(uint16_t dataId, uint8_t  data) { write(dataId, 1, &data); }
     void write(uint16_t dataId, uint16_t data) { write(dataId, 1, &data); }
     void write(uint16_t dataId, uint32_t data) { write(dataId, 1, &data); }
@@ -65,9 +99,16 @@ public:
     void write(uint16_t dataId, double   data) { write(dataId, 1, &data); }
     void write(uint16_t dataId, char     data) { write(dataId, 1, &data); }
 
-    /////writeTo//////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Array entry write
-    // Overloaded for each data type
+    /**
+     * @brief Send data to a specific IP address via UDP. The packet is not sent to this board's subscribers.
+     * Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_DRIVELEFTRIGHT_DATA_ID
+     * @param dataCount How many elements are in the data array.
+     * @param data The data to send.
+     * @param ip The destination IP for this packet.
+     * @param port The destination port for this packet. Default is RC_ROVECOMM_ETHERNET_UDP_PORT
+     */
     void writeTo(uint16_t dataId, uint16_t dataCount, const uint8_t  *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { UDP.writeTo(dataId, dataCount, data, ip, port); }
     void writeTo(uint16_t dataId, uint16_t dataCount, const uint16_t *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { UDP.writeTo(dataId, dataCount, data, ip, port); }
     void writeTo(uint16_t dataId, uint16_t dataCount, const uint32_t *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { UDP.writeTo(dataId, dataCount, data, ip, port); }
@@ -77,8 +118,16 @@ public:
     void writeTo(uint16_t dataId, uint16_t dataCount, const float    *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { UDP.writeTo(dataId, dataCount, data, ip, port); }
     void writeTo(uint16_t dataId, uint16_t dataCount, const double   *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { UDP.writeTo(dataId, dataCount, data, ip, port); }
     void writeTo(uint16_t dataId, uint16_t dataCount, const char     *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { UDP.writeTo(dataId, dataCount, data, ip, port); }
-    // Single-value writeTo
-    // Overloaded for each data type
+
+    /**
+     * @brief Convenience method for sending a single value to a specific IP address via UDP.
+     * The packet is not sent to this board's subscribers. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_WATCHDOGOVERRIDE_DATA_ID
+     * @param data The datum to send.
+     * @param ip The destination IP for this packet.
+     * @param port The destination port for this packet. Default is RC_ROVECOMM_ETHERNET_UDP_PORT
+     */
     void writeTo(uint16_t dataId, uint8_t  data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { writeTo(dataId, 1, &data, ip, port); }
     void writeTo(uint16_t dataId, uint16_t data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { writeTo(dataId, 1, &data, ip, port); }
     void writeTo(uint16_t dataId, uint32_t data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { writeTo(dataId, 1, &data, ip, port); }
@@ -89,9 +138,13 @@ public:
     void writeTo(uint16_t dataId, double   data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { writeTo(dataId, 1, &data, ip, port); }
     void writeTo(uint16_t dataId, char     data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_UDP_PORT) { writeTo(dataId, 1, &data, ip, port); }
 
-    /////writeReliable////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Array entry write which ensures delivery
-    // Overloaded for each data type
+    /**
+     * @brief Send data to all subscribers via TCP. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_DRIVELEFTRIGHT_DATA_ID
+     * @param dataCount How many elements are in the data array.
+     * @param data The data to send.
+     */
     void writeReliable(uint16_t dataId, uint16_t dataCount, const uint8_t  *data) { TCP.write(dataId, dataCount, data); }
     void writeReliable(uint16_t dataId, uint16_t dataCount, const uint16_t *data) { TCP.write(dataId, dataCount, data); }
     void writeReliable(uint16_t dataId, uint16_t dataCount, const uint32_t *data) { TCP.write(dataId, dataCount, data); }
@@ -101,8 +154,13 @@ public:
     void writeReliable(uint16_t dataId, uint16_t dataCount, const float    *data) { TCP.write(dataId, dataCount, data); }
     void writeReliable(uint16_t dataId, uint16_t dataCount, const double   *data) { TCP.write(dataId, dataCount, data); }
     void writeReliable(uint16_t dataId, uint16_t dataCount, const char     *data) { TCP.write(dataId, dataCount, data); }
-    // Single-value write which ensures delivery
-    // Overloaded for each data type
+
+    /**
+     * @brief Convenience method for sending a single value to all subscribers via TCP. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_WATCHDOGOVERRIDE_DATA_ID
+     * @param data The datum to send.
+     */
     void writeReliable(uint16_t dataId, uint8_t  data) { writeReliable(dataId, 1, &data); }
     void writeReliable(uint16_t dataId, uint16_t data) { writeReliable(dataId, 1, &data); }
     void writeReliable(uint16_t dataId, uint32_t data) { writeReliable(dataId, 1, &data); }
@@ -112,10 +170,17 @@ public:
     void writeReliable(uint16_t dataId, float    data) { writeReliable(dataId, 1, &data); }
     void writeReliable(uint16_t dataId, double   data) { writeReliable(dataId, 1, &data); }
     void writeReliable(uint16_t dataId, char     data) { writeReliable(dataId, 1, &data); }
-    
-    /////writeToReliable////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Array entry write which ensures delivery
-    // Overloaded for each data type
+
+    /**
+     * @brief Send data to a specific IP address via TCP. The packet is not sent to this board's subscribers.
+     * Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_DRIVELEFTRIGHT_DATA_ID
+     * @param dataCount How many elements are in the data array.
+     * @param data The data to send.
+     * @param ip The destination IP for this packet.
+     * @param port The destination port for this packet. Default is RC_ROVECOMM_ETHERNET_TCP_PORT
+     */
     void writeToReliable(uint16_t dataId, uint16_t dataCount, const uint8_t  *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { TCP.writeTo(dataId, dataCount, data, ip, port); }
     void writeToReliable(uint16_t dataId, uint16_t dataCount, const uint16_t *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { TCP.writeTo(dataId, dataCount, data, ip, port); }
     void writeToReliable(uint16_t dataId, uint16_t dataCount, const uint32_t *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { TCP.writeTo(dataId, dataCount, data, ip, port); }
@@ -125,8 +190,16 @@ public:
     void writeToReliable(uint16_t dataId, uint16_t dataCount, const float    *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { TCP.writeTo(dataId, dataCount, data, ip, port); }
     void writeToReliable(uint16_t dataId, uint16_t dataCount, const double   *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { TCP.writeTo(dataId, dataCount, data, ip, port); }
     void writeToReliable(uint16_t dataId, uint16_t dataCount, const char     *data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { TCP.writeTo(dataId, dataCount, data, ip, port); }
-    // Single-value writeTo which ensures delivery
-    // Overloaded for each data type
+
+    /**
+     * @brief Convenience method for sending a single value to a specific IP address via TCP.
+     * The packet is not sent to this board's subscribers. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_WATCHDOGOVERRIDE_DATA_ID
+     * @param data The datum to send.
+     * @param ip The destination IP for this packet.
+     * @param port The destination port for this packet. Default is RC_ROVECOMM_ETHERNET_TCP_PORT
+     */
     void writeToReliable(uint16_t dataId, uint8_t  data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { writeToReliable(dataId, 1, &data, ip, port); }
     void writeToReliable(uint16_t dataId, uint16_t data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { writeToReliable(dataId, 1, &data, ip, port); }
     void writeToReliable(uint16_t dataId, uint32_t data, IPAddress ip, uint16_t port=RC_ROVECOMM_ETHERNET_TCP_PORT) { writeToReliable(dataId, 1, &data, ip, port); }
---
+++ src/RoveCommInterface.h
@@ -0,0 +1,93 @@
+#ifndef ROVECOMM_INTERFACE_H
+#define ROVECOMM_INTERFACE_H
+
+#include "../RoveCommManifest.h"
+#include "../RoveCommPacket.h"
+
+/**
+ * @brief An interface used internally by RoveComm to share boilerplate between
+ * RoveCommEthernet, RoveCommWiFi, etc
+ * 
+ */
+class RoveCommInterface {
+    /**
+     * @brief Send data to all subscribers via UDP. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_DRIVELEFTRIGHT_DATA_ID
+     * @param dataCount How many elements are in the data array.
+     * @param data The data to send.
+     */
+    void write(uint16_t dataId, uint16_t dataCount, const uint8_t  *data) { _write(rovecomm::UINT8_T,  dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const uint16_t *data) { _write(rovecomm::UINT16_T, dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const uint32_t *data) { _write(rovecomm::UINT32_T, dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const int8_t   *data) { _write(rovecomm::INT8_T,   dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const int16_t  *data) { _write(rovecomm::INT16_T,  dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const int32_t  *data) { _write(rovecomm::INT32_T,  dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const float    *data) { _write(rovecomm::FLOAT,    dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const double   *data) { _write(rovecomm::DOUBLE,   dataId, dataCount, data); }
+    void write(uint16_t dataId, uint16_t dataCount, const char     *data) { _write(rovecomm::CHAR,     dataId, dataCount, data); }
+
+    /**
+     * @brief Convenience method for sending a single value to all subscribers via UDP. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_WATCHDOGOVERRIDE_DATA_ID
+     * @param data The datum to send.
+     */
+    void write(uint16_t dataId, uint16_t dataCount, uint8_t  data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, uint16_t data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, uint32_t data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, int8_t   data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, int16_t  data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, int32_t  data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, float    data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, double   data) { write(dataId, 1, &data); }
+    void write(uint16_t dataId, uint16_t dataCount, char     data) { write(dataId, 1, &data); }
+
+    /**
+     * @brief Send data to a specific IP address via UDP. The packet is not sent to this board's subscribers.
+     * Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_DRIVELEFTRIGHT_DATA_ID
+     * @param dataCount How many elements are in the data array.
+     * @param data The data to send.
+     * @param ip The destination IP for this packet.
+     * @param port The destination port for this packet.
+     */
+    void writeTo(uint16_t dataId, uint16_t dataCount, const uint8_t  *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::UINT8_T,  dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const uint16_t *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::UINT16_T, dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const uint32_t *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::UINT32_T, dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const int8_t   *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::INT8_T,   dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const int16_t  *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::INT16_T,  dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const int32_t  *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::INT32_T,  dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const float    *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::FLOAT,    dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const double   *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::DOUBLE,   dataId, dataCount, data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t dataCount, const char     *data, IPAddress ip, uint16_t port) { _writeTo(rovecomm::CHAR,     dataId, dataCount, data, ip, port); }
+
+    /**
+     * @brief Convenience method for sending a single value to a specific IP address via UDP.
+     * The packet is not sent to this board's subscribers. Overloaded for each data type.
+     * 
+     * @param dataId ID of this packet. e.g. RC_COREBOARD_WATCHDOGOVERRIDE_DATA_ID
+     * @param data The datum to send.
+     * @param ip The destination IP for this packet.
+     * @param port The destination port for this packet.
+     */
+    void writeTo(uint16_t dataId, uint8_t  data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, uint16_t data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, uint32_t data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, int8_t   data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, int16_t  data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, int32_t  data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, float    data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, double   data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+    void writeTo(uint16_t dataId, char     data, IPAddress ip, uint16_t port) { writeTo(dataId, 1, &data, ip, port); }
+
+protected:
+
+    virtual void _write(const rovecomm::data_type_t dataType, const uint16_t dataId, const uint16_t dataCount, const void *data) = 0;
+    virtual void _writeTo(const rovecomm::data_type_t dataType, const uint16_t dataId, const uint16_t dataCount, const void *data,
+                          const IPAddress ip, const uint16_t port) = 0;
+
+};
+
+#endif // ROVECOMM_INTERFACE_H
